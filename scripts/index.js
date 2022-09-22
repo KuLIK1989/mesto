a51dde6c74defe69a85567f7a5877d3e25ca3647
Aleksandr <aleksandr_kulikov89@vk.com>
2022-09-22 15:23:03 +0300

    feat:добавить js

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+// получить элементы
+const popeupOpenButton = document.querySelector('.profile__open-popup');
+const popup = document.querySelector('.popup');
+const popUpCloseButton = popup.querySelector('.popup__close-button');
+const popeupSaveButton = popup.querySelector('.form__save-button');
+let formElement = popup.querySelector('.form');
+let nameInput = formElement.querySelector('#name');
+let jobInput = formElement.querySelector('#status');
+const userName = document.querySelector('.profile__name');
+const aboutUser = document.querySelector('.profile__status');
+
+//получить функцию
+function openPopup() {
+   popup.classList.add('popup__opened');
+}
+function closePopup() {
+   popup.classList.remove('popup__opened');
+}
+function savePopup() {
+   popup.classList.remove('popup__opened');
+}
+
+function formSubmitHandler(evt) {
+   evt.preventDefault();
+   userName.textContent = nameInput.value;
+   aboutUser.textContent = jobInput.value;   
+}
+
+// добавить слушатели 
+popeupOpenButton.addEventListener('click', openPopup);
+popUpCloseButton.addEventListener('click', closePopup);
+popeupSaveButton.addEventListener('click', savePopup);
+formElement.addEventListener('submit', formSubmitHandler);
+
