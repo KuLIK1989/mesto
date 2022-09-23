8eb6e41b6a0b2d79955b4df6fbd0bffa3a533ce9
Aleksandr <aleksandr_kulikov89@vk.com>
2022-09-23 12:26:56 +0300

    fix:ревью 1 исправление ошибок

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,32 @@
 // получить элементы
-const popeupOpenButton = document.querySelector('.profile__open-popup');
+const popeupOpenButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popUpCloseButton = popup.querySelector('.popup__close-button');
-const popeupSaveButton = popup.querySelector('.form__save-button');
-let formElement = popup.querySelector('.form');
+const popeupSaveButton = popup.querySelector('#form-save-btn');
+let formElement = popup.querySelector('#form-edit');
 let nameInput = formElement.querySelector('#name');
 let jobInput = formElement.querySelector('#status');
 const userName = document.querySelector('.profile__name');
 const aboutUser = document.querySelector('.profile__status');
+console.log(formElement);
 
 //получить функцию
 function openPopup() {
-   popup.classList.add('popup__opened');
+   popup.classList.add('popup_opened');
 }
 function closePopup() {
-   popup.classList.remove('popup__opened');
-}
-function savePopup() {
-   popup.classList.remove('popup__opened');
+   popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
    evt.preventDefault();
    userName.textContent = nameInput.value;
    aboutUser.textContent = jobInput.value;
+   closePopup()
 }
 
 // добавить слушатели 
 popeupOpenButton.addEventListener('click', openPopup);
 popUpCloseButton.addEventListener('click', closePopup);
-popeupSaveButton.addEventListener('click', savePopup);
 formElement.addEventListener('submit', formSubmitHandler);
 
